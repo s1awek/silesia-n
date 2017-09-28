70d328efb4a0426abd9df1f22e7d0365ec53674b
AG <elvizky@gmail.com>
2017-09-29 01:19:18 +0200

    [VP] Added changes for top menu to child theme style.css and some code to functions.php to make possible to upload svg images.
    X-VP-Version: 4.0-beta
    X-VP-Environment: default1

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -13,3 +13,9 @@ endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
 
 // END ENQUEUE PARENT ACTION
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
